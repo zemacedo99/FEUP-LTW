949e35f75e5d58422936fe97a07f99024ec000f8
Zé Macedo <up201705226@fe.up.pt>
2021-12-23 17:43:43 +0000

    game config, we may need localstorage

--- js/game.js
+++ js/game.js
@@ -1,17 +1,34 @@
 let game;
 
+
+
+
+var btnNewGame = document.getElementById("btn-newgame");
+
+
 class Game {
-    constructor(num_holes, num_seeds) {
-        this.num_holes = num_holes;
-        this.num_seeds = num_seeds;
+    constructor() {
         this.startGame();
     }
 
     startGame() {
+        this.update_config();
         this.board = new Board("board", this.num_holes, this.num_seeds);
     }
+
+    update_config()
+    {
+        this.num_holes = document.getElementById("number_holes").value;
+        this.num_seeds = document.getElementById("number_seeds").value;
+    }
+
 }
 
 window.onload = function () {
-    game = new Game(7, 5);
+    game = new Game();
+}
+
+
+btnNewGame.onclick = function() {
+	window.location.reload();
 }
