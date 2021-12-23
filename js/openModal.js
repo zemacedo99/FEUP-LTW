674bf83bd607b0e8fdf89d53552fccef92e78d12
Zé Macedo <up201705226@fe.up.pt>
2021-12-23 15:18:09 +0000

    css wip

---
+++ js/openModal.js
@@ -0,0 +1,53 @@
+
+var rulesModal = document.getElementById("myrulesModal");
+var configModal = document.getElementById("myConfigModal");
+var leaderBordModal = document.getElementById("myLeaderBordModal");
+
+var btnRules = document.getElementById("btnRules");
+var btnConfig = document.getElementById("btnConfig");
+var btnLeaderBord = document.getElementById("btnLeaderBord");
+
+var closerules = document.getElementById("closeRules");
+var closeConfig = document.getElementById("closeConfig");
+var closeLeaderBoard = document.getElementById("closeLeaderBoard");
+
+btnRules.onclick = function() {
+  rulesModal.style.display = "flex";
+}
+
+btnConfig.onclick = function() {
+    configModal.style.display = "flex";
+}
+
+btnLeaderBord.onclick = function() {
+    leaderBordModal.style.display = "flex";
+}
+
+
+closerules.onclick = function() {
+    rulesModal.style.display = "none";
+}
+
+closeConfig.onclick = function() {
+    configModal.style.display = "none";
+}
+
+closeLeaderBoard.onclick = function() {
+    leaderBordModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside, close it
+window.onclick = function(event) {
+    if (event.target == rulesModal)
+    {
+        rulesModal.style.display = "none";
+    }
+    if (event.target == configModal) 
+    {
+        configModal.style.display = "none";
+    }
+    if (event.target == leaderBordModal) 
+    {
+        leaderBordModal.style.display = "none";
+    }
+}
