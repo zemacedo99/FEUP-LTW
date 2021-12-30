22ba325508cc7205c1c8978f25f06783a29dab68
Zé Macedo <up201705226@fe.up.pt>
2021-12-30 19:56:38 +0000

    empty the hole, but needs sort

--- js/hole.js
+++ js/hole.js
@@ -29,6 +29,33 @@ class Hole {
         }
     }
 
+    updateHole() {
+        //add child hole to parent hole
+        document.getElementById(this.id).remove();
+        
+        const row = document.getElementById("row" + (this.row).toString());
+        let hole = document.createElement("div");
+        hole.setAttribute("class", "hole");
+        hole.setAttribute("id", this.id);
+        row.appendChild(hole);
+
+        //now fill the hole with seeds
+        for (let i = 0; i < this.num_seeds; i++) {
+            let seed = new Seed(this.row, this.hole);
+            this.seeds_list[i] = seed;
+        }
+        
+    }
+
+    set_num_seeds(n) {
+        this.num_seeds = n.toString();
+    }
+
+    empty_seeds_list()
+    {
+        this.seeds_list = [];
+    }
+
 
     //addSeed() function to add a seed in hole
 
@@ -39,7 +66,9 @@ class Hole {
 
 function sow(hole)
 {       
-    // let id = "hole" + row.toString() + hole.toString();
     console.log(hole)
-    console.log(hole.hole)
+    hole.set_num_seeds(0);
+    hole.empty_seeds_list();
+    hole.updateHole();
+    console.log(hole)
 }
