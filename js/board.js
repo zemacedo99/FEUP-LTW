dfcc9f06ba674a60e8c340187627b5c026188eee
Zé Macedo <up201705226@fe.up.pt>
2022-01-01 21:52:09 +0000

    sow to the right

--- js/board.js
+++ js/board.js
@@ -45,33 +45,16 @@ class Board {
         let row = this.rows_list[rowIndex];
         let sow_hole_index = holeIndex;
 
-
-
-        while(hole.harvested_seeds != 0)
+        
+        if(rowIndex == 1)
         {
-            sow_hole_index = sow_hole_index + 1;
-
-            if(sow_hole_index <= this.num_holes)
-            {
-                let next_hole = row.holes_list[sow_hole_index];
-                next_hole.addSeed();
-                hole.harvested_seeds--;
-            }
-            else
-            {
-                let storage = this.storages_list[rowIndex];
-
-                //add seed to storage
-                storage.addSeed(); 
-                //change row
-                rowIndex = rowIndex ? 0 : 1 
-
-                hole.harvested_seeds--;
-                break;
-            }
+            this.sow_to_the_right(row,rowIndex,sow_hole_index,hole);
         }
-
-        // console.log(hole.harvested_seeds)
+        else
+        {
+            console.log("wip")
+        }
+        
 
         // for(let i = this.num_holes - 1; i < this.num_holes - hole.harvested_seeds ; i--)
         // {
@@ -85,6 +68,33 @@ class Board {
  
 
         hole.reaping = false;
+    }
+
+    sow_to_the_right(row,rowIndex,sow_hole_index,hole)
+    {
+        while(hole.harvested_seeds != 0)
+        {
+
+
+            sow_hole_index++;
+
+            if(sow_hole_index <= this.num_holes)
+            {
+                let next_hole = row.holes_list[sow_hole_index];
+                next_hole.addSeed();
+            }
+            else
+            {
+                let storage = this.storages_list[rowIndex];
+
+                //add seed to storage
+                storage.addSeed(); 
+                //change row
+                rowIndex = rowIndex ? 0 : 1 
+                break;
+            }
+            hole.harvested_seeds--;
+        }
     }
 
     
