c77a174acc54375ea9dfdc638a16401a808b9fff
Joao Vasconcelos <up201504397@fe.up.pt>
2022-01-21 15:23:15 +0000

    solved problem in ai suggestion

--- js/ai.js
+++ js/ai.js
@@ -1,17 +1,8 @@
 function aiPlay(ai_level, row) {
-    console.log(ai_level)
     let chosen_hole;
 
     if (ai_level == 1) {
-        let found_play = true;
-
-        while (found_play) {
-            chosen_hole = Math.ceil(Math.random() * (row.holes_list.length));
-            if (row.holes_list[chosen_hole].num_seeds != 0) {
-                found_play = false;
-            }
-
-        }
+        chosen_hole = aiBasic(row);
     }
     else if (ai_level == 2) {
         chosen_hole = aiGreedy(row)
@@ -20,20 +11,37 @@ function aiPlay(ai_level, row) {
     return chosen_hole;
 }
 
+function aiBasic(row) {
+    let chosen_hole;
+    let found_play = true;
+
+    while (found_play) {
+        chosen_hole = Math.ceil(Math.random() * (row.holes_list.length + 1));
+
+        if (row.holes_list[chosen_hole].num_seeds != 0) {
+            found_play = false;
+        }
+    }
+
+    return chosen_hole;
+}
+
 function aiGreedy(row) {
     //for loop que escolhe um hole que tenha o id igual ao número de seed, para assim deixar uma semaente no storage e poder jogar outra vez
     //
-    for (let i = 1; i <= row.holes_list.length; i++) {
+    for (let i = 1; i < row.holes_list.length; i++) {
         if (row.holes_list[i].num_seeds == i) {
-            return i - 1;
+            console.log("suggestion in smart is " + i);
+            return i;
         }
     }
     //nao descrobrindo uma jogada no primeiro for loop
     //
     //for loop para descrobrir o hole mais próximo do storage com sementes e fazer a jogada nesse hole
-    for (let i = 1; i <= row.holes_list.length; i++) {
+    for (let i = 1; i < row.holes_list.length; i++) {
         if (row.holes_list[i].num_seeds != 0) {
-            return i - 1;
+            console.log("suggestion in smart random is " + i);
+            return i;
         }
     }
 }
